c71a07353bb83d99c8bed5c6c96b371308fe9d3d
Derek <dereklee@downtothis.com>
2018-06-27 12:38:00 +1000

    First passing web spec for displaying invalid.

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,46 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+
+class RPSApp extends React.Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    submitHandler() {
+        this.setState({
+            gameResult: 'INVALID!',
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.gameResult}
+                <button onClick={this.submitHandler.bind(this)}>Play</button>
+            </div>
+        )
+    }
+}
+
+describe('play form', function () {
+    describe('when the play use case tells the UI that the input is invalid', () => {
+        it('tells the user that their input is invalid', () => {
+            const domFixture = document.createElement('div')
+            document.body.appendChild(domFixture)
+
+            const alwaysInvalidRound = {
+                play: (p1, p2, observer) => observer.invalidThrow()
+            }
+
+            ReactDOM.render(
+                <RPSApp rounds={alwaysInvalidRound}/>,
+                domFixture
+            )
+
+            expect(domFixture.innerText).not.toContain('INVALID!')
+            document.querySelector('button').click()
+            expect(domFixture.innerText).toContain('INVALID!')
+        })
     })
 })
