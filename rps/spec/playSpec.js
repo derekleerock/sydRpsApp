4a54aefad83ee57ec8ae5dc247cd7c9022fd95e3
Derek <dereklee@downtothis.com>
2018-06-27 11:43:32 +1000

    Method object; compose method refactorings; introduce constants.

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,18 +1,43 @@
 function Round() {
     this.play = (player1Throw, player2Throw, observer) => {
-        if (['rock', 'scissors', 'paper'].includes(player1Throw) === false ||
-            ['rock', 'scissors', 'paper'].includes(player2Throw) === false) {
+        new PlayRound(player1Throw, player2Throw, observer).process()
+    }
+}
+
+function PlayRound(player1Throw, player2Throw, observer) {
+    this.process = () => {
+        if (invalid(player1Throw) || invalid(player2Throw)) {
             observer.invalid()
-        } else if (player1Throw === player2Throw) {
+        } else if (tie()) {
             observer.tie()
-        } else if (player1Throw === 'rock' && player2Throw === 'scissors' ||
-            player1Throw === 'scissors' && player2Throw === 'paper' ||
-            player1Throw === 'paper' && player2Throw === 'rock') {
+        } else if (player1WinsScenarios()) {
             observer.player1Wins()
         } else {
             observer.player2Wins()
         }
     }
+
+    function tie() {
+        return player1Throw === player2Throw
+    }
+
+    function player1WinsScenarios() {
+        return player1Throw === THROW.ROCK && player2Throw === THROW.SCISSORS ||
+            player1Throw === THROW.SCISSORS && player2Throw === THROW.PAPER ||
+            player1Throw === THROW.PAPER && player2Throw === THROW.ROCK
+    }
+
+    function invalid(playerThrow) {
+        return VALID_THROWS.includes(playerThrow) === false
+    }
+    
+    const THROW = {
+        ROCK: 'rock',
+        SCISSORS: 'scissors',
+        PAPER: 'paper'
+    }
+
+    const VALID_THROWS = [THROW.ROCK, THROW.SCISSORS, THROW.PAPER]
 }
 
 describe('play', () => {
